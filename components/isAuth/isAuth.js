fbfb62b65e856c1db60dc8f540dec996a0b3a427
Elizabeth Sheremet <elizabethsheremet@Noutbuk-Elizabeth.local>
2024-08-11 16:52:43 -0400

    add rate context

--- components/isAuth/isAuth.js
+++ components/isAuth/isAuth.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import AppLayout from "../AppLayout/AppLayout";
 import Login from "../Login/Login";
 import Loading from "../Loading/Loading";
+import { RateProvider } from "../context/RateContext";
 
 const IsAuth = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
@@ -23,7 +24,7 @@ const IsAuth = ({ children }) => {
     }
   
     return (
-      <>{isAuthenticated ? <AppLayout>{children}</AppLayout> : <Login  /> }</>
+      <RateProvider>{isAuthenticated ? <AppLayout>{children}</AppLayout> : <Login  /> }</RateProvider>
     );
 }
 
