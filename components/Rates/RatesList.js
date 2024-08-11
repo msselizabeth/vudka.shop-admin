4392e2f1a987c4fb5ba821383b73f22dd68f667e
Elizabeth Sheremet <elizabethsheremet@Noutbuk-Elizabeth.local>
2024-08-11 16:53:50 -0400

    fix component for context

--- components/Rates/RatesList.js
+++ components/Rates/RatesList.js
@@ -1,86 +1,44 @@
 "use client";
-import { useState, useEffect } from "react";
-import axios from "axios";
-
+import React, { useState, useEffect, useContext } from "react";
 import styles from "./RatesList.module.css";
 import UpdateInput from "../Exchange/UpdateInput";
 import ToChangeButton from "../Exchange/ToChangeButton";
 import InnerLoad from "../InnerLoad/InnerLoad";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileInvoiceDollar } from "@fortawesome/free-solid-svg-icons";
-
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+// Импортируем контекст
+import { RateContext } from "../context/RateContext";
+
 const RatesList = () => {
-  const [mainRate, setMainRate] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [editTarget, setEditTarget] = useState(null);
-  const [tempRate, setTempRate] = useState(0);
-  const token = localStorage.getItem("token");
+  const [isEditing, setIsEditing] = useState(false);
+  const [tempRate, setTempRate] = useState("");
+  const { rate, updateRate, isLoading, error } = useContext(RateContext);
 
-  // Function to fetch the main rate from the server
-  const fetchMainRate = async () => {
-    try {
-      setIsLoading(true);
-      setError(null);
-
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/exchange`,
-        {
-          params: {
-            target: "main",
-          },
-        }
-      );
-        setMainRate(response.data.exchange.value);
-        
-    } catch (error) {
-      setError("Не вдалося завантажити поточний курс.");
-        toast.error("Не вдалося завантажити поточний курс.");
-        console.error("Помилка:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
+  // Устанавливаем курс в качестве начального значения для tempRate
   useEffect(() => {
-    fetchMainRate();
-  }, []);
+    if (rate) {
+      setTempRate(rate.toString());
+    }
+  }, [rate]);
 
-  // Function to save the updated rate
+  // Функция для сохранения обновленного курса
   const saveRate = async () => {
-
     try {
-      setIsLoading(true);
-      setError(null);
-
-      await axios.patch(
-        `${process.env.NEXT_PUBLIC_API_URL}/exchange`,
-        { target: "main", value: tempRate },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-
-      setEditTarget(null);
+      await updateRate(tempRate);
+      setIsEditing(false);
       toast.success("Курс оновлено успішно.");
-      fetchMainRate();
     } catch (error) {
-        setError("Не вдалось зберегти курс");
-        toast.error("Помилка при оновленні курсу.");
+      toast.error("Помилка при оновленні курса.");
       console.error("Помилка:", error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
+  // Обработчик изменения значения в поле ввода
   const handleInputChange = (e) => {
     setTempRate(e.target.value);
-  };
-
-  const startEditing = (target, currentValue) => {
-    setEditTarget(target);
-    setTempRate(currentValue.toString());
   };
 
   return (
@@ -94,18 +52,18 @@ const RatesList = () => {
         <InnerLoad size={25} color={"#03464F"} text={"Завантаження курса..."} />
       ) : (
         <div className={styles.ratesList}>
-          {editTarget === "main" ? (
+          {isEditing ? (
             <UpdateInput
               value={tempRate}
               onChange={handleInputChange}
               disabled={isLoading}
               saveRate={saveRate}
-              setEditTarget={setEditTarget}
+              setEditTarget={() => setIsEditing(false)}
             />
           ) : (
             <div className={styles.rate}>
-              <span className={styles.rateValue}>{mainRate} грн/1$</span>
-              <ToChangeButton onClick={() => startEditing("main", mainRate)} />
+              <span className={styles.rateValue}>{rate} грн/1$</span>
+              <ToChangeButton onClick={() => setIsEditing(true)} />
             </div>
           )}
         </div>
