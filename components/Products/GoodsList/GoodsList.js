d31acf5d2394da94dc5fc8ab52510d17bc800949
Elizabeth Sheremet <elizabethsheremet@Noutbuk-Elizabeth.local>
2024-08-26 16:29:36 -0400

    add delete func + add table to introduce info

--- components/Products/GoodsList/GoodsList.js
+++ components/Products/GoodsList/GoodsList.js
@@ -3,9 +3,37 @@ import styles from "./GoodsList.module.css";
 import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
 import { RateContext } from "@/components/context/RateContext";
+import { calcPrice } from "@/middlewares/calcPrice";
+import YesComponent from "@/components/Highlights/YesComponent";
+import NoComponent from "@/components/Highlights/NoComponent";
+import EditButton from "@/components/Buttons/EditButton";
+import DeleteButton from "@/components/Buttons/DeleteButton";
+import ConfirmationModal from "@/components/Modal/ConfirmationModal";
 
 const GoodsList = ({ goods, goodFields }) => {
   const { rate } = useContext(RateContext);
+
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const handleDeleteClick = (item) => {
+    setCurrentItem(item);
+    setIsConfirmModalOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    // if (currentItem && onDelete) {
+    //   onDelete(currentItem._id); // Передаем ID элемента для удаления
+    // }
+    setIsConfirmModalOpen(false);
+    setCurrentItem(null);
+  };
+
+  const handleCancelDelete = () => {
+    setIsConfirmModalOpen(false);
+    setCurrentItem(null);
+  };
 
   return (
     <ul className={styles.goodsList}>
@@ -40,21 +68,82 @@ const GoodsList = ({ goods, goodFields }) => {
                   href={"/"}
                   className={styles.goodLink}
                 >{`${name} ${brand} ${series} ${model}`}</Link>
-                <p className={styles.stock}>
-                  {stock > 0 ? `На складі: ${stock}шт` : "немає в наявності"}
-                </p>
-                <p className={styles.goodArticle}>{`Артикул: ${item}`}</p>
+              <div className={styles.buttonsContainer}>
+        <EditButton onClick={"onEdit"} />
+                  <DeleteButton onClick={() =>
+                        handleDeleteClick({
+                          _id,
+                          name,
+                          brand,
+                          series,
+                          model,
+                        })
+                      } />
+                  {isConfirmModalOpen && (
+                    <ConfirmationModal
+                      element={`${currentItem.name} ${currentItem.brand} ${currentItem.series} ${currentItem.model}`}
+          onConfirm={handleConfirmDelete}
+          onCancel={handleCancelDelete}
+        />
+      )}
+      </div>
+               
               </div>
-              <p>{`Ціна: ${price}$ - ${Math.ceil(
-                price * parseFloat(rate)
-              )}грн`}</p>
-              {`Знижку активовано - ${promotion ? "так" : "ні"}`}
-              {promotion &&
-                `Ціна(акційна): ${price}$ - ${Math.ceil(price * discount * parseFloat(rate))}грн`}
-              <p>{`Ціна(розпродаж): ${salePriceMain}$ - ${Math.ceil(
-                salePriceMain * parseFloat(rate)
-              )}грн`}</p>
-              <p>{`Участь у розпродажі: ${sale ? "Так" : "Ні"}`}</p>
+              <table className={styles.priceTable}>
+                <tr>
+                  <th>Параметр</th>
+                  <th>Значення</th>
+                </tr>
+                <tr>
+                  <td>Артикул</td>
+                  <td>{item}</td>
+                </tr>
+                <tr>
+                <td>На складі</td>
+                <td>{stock > 0 ? `${stock} шт` : "немає в наявності"}</td>
+                </tr>
+                <tr>
+                  <td>Ціна</td>
+                  <td>{`${price}$ - ${calcPrice(
+                    promotion,
+                    price,
+                    discount,
+                    rate
+                  )} грн`}</td>
+                </tr>
+                <tr>
+                  <td>Знижку активовано</td>
+                  <td>
+                    {promotion ? <YesComponent/> : <NoComponent/>}
+                  </td>
+                </tr>
+                {promotion && (
+                  <tr>
+                    <td>Ціна (акційна)</td>
+                    <td>{`${price}$ - ${calcPrice(
+                      promotion,
+                      price,
+                      discount,
+                      rate
+                    )} грн`}</td>
+                  </tr>
+                )}
+                <tr>
+                  <td>Участь у розпродажі</td>
+                  <td >
+                    {sale ? <YesComponent/> : <NoComponent/>}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Ціна (розпродаж)</td>
+                  <td>{`${salePriceMain}$ - ${calcPrice(
+                    promotion,
+                    salePriceMain,
+                    discount,
+                    rate
+                  )} грн`}</td>
+                </tr>
+              </table>
             </div>
           </li>
         )
