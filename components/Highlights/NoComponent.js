3ba5cb9bf6f86d432c954fc13d44ddf034978d29
Elizabeth Sheremet <elizabethsheremet@Noutbuk-Elizabeth.local>
2024-08-26 16:33:21 -0400

    add text component to highlight promotiom and sales mode (active/no active)

---
+++ components/Highlights/NoComponent.js
@@ -0,0 +1,16 @@
+
+
+const NoComponent = () => (
+    <span style={{ 
+      backgroundColor: '#f8d7da', 
+      color: '#721c24', 
+      padding: '5px 10px', 
+      borderRadius: '5px',
+      display: 'inline-block' 
+    }}>
+      Ні
+    </span>
+  );
+
+  
+export default NoComponent;
---
+++ components/Highlights/YesComponent.js
@@ -0,0 +1,14 @@
+
+const YesComponent = () => (
+    <span style={{ 
+      backgroundColor: '#d4edda', 
+      color: '#155724', 
+      padding: '5px 10px', 
+      borderRadius: '5px',
+      display: 'inline-block' 
+    }}>
+      Так
+    </span>
+);
+  
+export default YesComponent;
