0040f1d52d1caf20ba7b8e70a01370e5384acbfc
Elizabeth Sheremet <elizabethsheremet@noutbuklizabeth.home>
2024-09-02 17:07:22 -0400

    fix prcie calc func

--- middlewares/calcPrice.js
+++ middlewares/calcPrice.js
@@ -1,12 +1,10 @@
-
-
 export const calcPrice = (promotion, price, discount, rate) => {
-    let result = 0;
-    if (promotion) {
-        result = Math.ceil(parseFloat(price) * discount * parseFloat(rate));
-    } else {
-        result = Math.ceil(parseFloat(price) * parseFloat(rate));
-    }
+  let result = 0;
+  if (promotion) {
+    result = Math.ceil(parseFloat(price) * (1 - parseFloat(discount) / 100) * parseFloat(rate));
+  } else {
+    result = Math.ceil(parseFloat(price) * parseFloat(rate));
+  }
 
-    return result;
-}
+  return result;
+};
